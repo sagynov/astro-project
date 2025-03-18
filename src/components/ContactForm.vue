477a062f2dd9b53d8ccc31e1019146cf657d9810
sagynov <kzxit99@gmail.com>
2025-03-18 12:16:34 +0500

    third commit

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import InputText from 'primevue/inputtext';
+import Textarea from 'primevue/textarea';
+import Button from 'primevue/button';
+import { ref } from 'vue';
+
+const name = ref(null);
+const email = ref(null);
+const message = ref(null);
+</script>
+
+<template>
+    <div class="grid sm:grid-cols-2 gap-4">
+        <InputText type="text" v-model="name" placeholder="Name" />
+        <InputText type="text" v-model="email" placeholder="Email" />
+        <Textarea v-model="message" class="col-span-2" rows="5" placeholder="Message" cols="30" />
+        <Button label="Send Message" class="col-span-2 !p-3" />
+    </div>
+</template>
