75af6d9cbb54e492148356a12128fcc066d7f0fa
sagynov <kzxit99@gmail.com>
2025-04-09 14:46:52 +0500

    slider added

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import Galleria from 'primevue/galleria';
+import Image from 'primevue/image';
 
 const props = defineProps<{
   data: any
@@ -18,20 +18,13 @@ const responsiveOptions = ref([
 </script>
 
 <template>
-    <div class="flex justify-center">
-        <Galleria class="w-full" :value="props.data.gallery" :responsiveOptions="responsiveOptions" :numVisible="5" containerStyle="max-width: 640px">
-            <template #item="slotProps">
-                <img :src="slotProps.item.src" :alt="slotProps.item.alt" style="width: 100%" />
-            </template>
-            <template #thumbnail="slotProps">
-                <img :src="slotProps.item.src" class="w-24" :alt="slotProps.item.alt" />
-            </template>
-        </Galleria>
+    <div class="mb-4">
+        <div class="font-medium"><span class="uppercase">{{ props.data.title }}</span></div> 
+        <div class="font-medium mt-2"><span>{{ props.data.description }}</span></div> 
     </div>
-    <div class=" mb-4">
-        <div class="font-medium">PROJECT NAME:</div> 
-        <div>{{ props.data.title }}</div>
-        <div class="font-medium mt-2">PROJECT DESCRIPTION:</div> 
-        <div>{{ props.data.description }}</div>
+    <div class="grid grid-cols-2 sm:grid-cols-4 gap-4">
+        <div v-for="item in props.data.gallery" :key="item.src">
+            <Image :src="item.src" alt="Image" width="300" height="300" preview />           
+        </div>
     </div>
 </template>
---
+++ src/components/ProjectList.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Carousel from 'primevue/carousel';
+
+const props = defineProps<{
+  projects: any
+}>()
+const responsiveOptions = ref([
+    {
+        breakpoint: '1400px',
+        numVisible: 2,
+        numScroll: 1
+    },
+    {
+        breakpoint: '1199px',
+        numVisible: 3,
+        numScroll: 1
+    },
+    {
+        breakpoint: '767px',
+        numVisible: 2,
+        numScroll: 1
+    },
+    {
+        breakpoint: '575px',
+        numVisible: 1,
+        numScroll: 1
+    }
+]);
+</script>
+
+<template>
+    <Carousel :showNavigators="false" :showIndicators="true" :value="projects" :numVisible="2" :numScroll="1" :responsiveOptions="responsiveOptions" circular :autoplayInterval="5000">
+        <template #item="slotProps">
+            <div class="project-item m-2 p-4 h-[350px] bg-center bg-cover" :style="'background-image: url('+slotProps.data.data.image+')'">
+                <a :href="'/projects/'+slotProps.data.id" class="project-title uppercase font-medium text-4xl text-gray-700">
+                    <div>{{slotProps.data.data.title}}</div>
+                </a>
+            </div>
+        </template>
+    </Carousel>
+</template>
