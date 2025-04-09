b5bc1be1a00a54e402af3afc5d4609bd95cd376f
sagynov <kzxit99@gmail.com>
2025-04-09 14:59:43 +0500

    slider added 2

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -33,7 +33,7 @@ const responsiveOptions = ref([
     <Carousel :showNavigators="false" :showIndicators="true" :value="projects" :numVisible="2" :numScroll="1" :responsiveOptions="responsiveOptions" circular :autoplayInterval="5000">
         <template #item="slotProps">
             <div class="project-item m-2 p-4 h-[250px] sm:h-[350px] bg-center bg-cover" :style="'background-image: url('+slotProps.data.data.image+')'">
-                <a :href="'/projects/'+slotProps.data.id" class="project-title uppercase font-medium text-4xl text-gray-700">
+                <a :href="'/projects/'+slotProps.data.id+'/'" class="project-title uppercase font-medium text-4xl text-gray-700">
                     <div>{{slotProps.data.data.title}}</div>
                 </a>
             </div>
