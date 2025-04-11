8f80888ddeecc4980601ec8ebc7124fe0f2f671c
sagynov <kzxit99@gmail.com>
2025-04-11 12:53:35 +0500

    hide description

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -20,7 +20,7 @@ const responsiveOptions = ref([
 <template>
     <div class="mb-4">
         <div class="font-medium"><span class="uppercase">{{ props.data.title }}</span></div> 
-        <div class="font-medium mt-2"><span>{{ props.data.description }}</span></div> 
+        <div class="font-medium mt-2" v-if="false"><span>{{ props.data.description }}</span></div> 
     </div>
     <div class="grid grid-cols-2 sm:grid-cols-4 gap-4">
         <div v-for="item in props.data.gallery" :key="item.src">
