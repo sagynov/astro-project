cb7003b2623d2f63ba444a27fa5e6fd1eb178ed2
sagynov <kzxit99@gmail.com>
2025-04-11 12:51:05 +0500

    added flex

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import Carousel from 'primevue/carousel';
+import ProjectItem from "./ProjectItem.vue";
 
 const props = defineProps<{
   projects: any
@@ -27,16 +28,24 @@ const responsiveOptions = ref([
         numScroll: 1
     }
 ]);
+
+const activeProject = ref();
+
 </script>
 
 <template>
     <Carousel :showNavigators="false" :showIndicators="true" :value="projects" :numVisible="2" :numScroll="1" :responsiveOptions="responsiveOptions" circular :autoplayInterval="5000">
         <template #item="slotProps">
             <div class="project-item m-2 p-4 h-[250px] sm:h-[350px] bg-center bg-cover" :style="'background-image: url('+slotProps.data.data.image+')'">
-                <a :href="'/projects/'+slotProps.data.id+'/'" class="project-title uppercase font-medium text-4xl text-gray-700">
+                <a @click="activeProject = slotProps.data.data" class="project-title uppercase font-medium text-4xl text-gray-700 cursor-pointer">
                     <div>{{slotProps.data.data.title}}</div>
                 </a>
             </div>
         </template>
     </Carousel>
+
+    
+    <div class="grid p-4 sm:p-12" v-if="activeProject">
+        <ProjectItem :data="activeProject" />
+    </div>
 </template>
